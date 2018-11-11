19df49e950dcd3446c73fd194e7f6ec35ec314d6
UrchinZ <2j.zy.zhang@gmail.com>
2018-11-11 14:34:43 -0500

    :)

--- doc/view_lane_pose.py
+++ doc/view_lane_pose.py
@@ -6,14 +6,16 @@ Created on Wed Nov  7 10:11:45 2018
 """
 import matplotlib.pyplot as plt
 
-filename = "record_phi.txt"
+filename = "record_phi_yellow.txt"
 mode = "r"
 record  = open(filename, mode) 
 record_lines = record.read().splitlines()
 seq_list = []
 time_list = []
+time_diff_list=[]
 d_list = []
 phi_list = []
+time = 0
 for i in range(int(len(record_lines)/13)):
     lines = record_lines[i*13:(i+1)*13]
     #record sequence
@@ -23,7 +25,10 @@ for i in range(int(len(record_lines)/13)):
     stamp = lines[3:5]
     sec = stamp[0].split(' ')[-1]
     nsec = stamp[1].split(' ')[-1]
-    time = int(sec)+int(nsec)/(10**9)
+    time_diff = float(int(sec))+float(int(nsec))/float((10**9))-time 
+    time = float(int(sec))+float(int(nsec))/float((10**9))
+    if i > 1:
+        time_diff_list.append(time_diff)
     time_list.append(time)
     #d value
     d = float(lines[6].split(' ')[-1])
@@ -49,6 +54,10 @@ plt.ylabel("time")
 plt.xlabel("sequence")
 plt.show()
 
+plt.plot(time_diff_list)
+plt.ylabel("time")
+plt.xlabel("sequence")
+plt.show()
 
 
 
